5d06791bd12a22cf52455b7e89e7dd259119d68b
Alec Sherwood <alecsherwood@macbookpro.mynetworksettings.com>
2025-01-15 12:42:29 -0500

    Adding some basic UI and updating the manifest

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
 console.log("Jumper extension has started")
+
+if(navigator.requestMIDIAccess){
+    navigator.requestMediaKeySystemAccess().then(midiAcquired, midiAcquisitionFailure);
+} else {
+    console.log("Cannot access MIDI information.")
+}
+
+function midiAcquired(midiAccess){
+    console.log("acquired Midi Access...")
+}
+
+function midiAcquisitionFailure(){
+    console.log("Cannot access MIDI information.")
+}
